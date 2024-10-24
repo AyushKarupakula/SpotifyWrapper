70e8ea6ab1e74d58be65648839da519c0b0b40f0
Daniel Choe <dchoe4377@gmail.com>
2024-10-24 12:37:27 -0400

    added acc page and acc delete function

--- userAuth/urls.py
+++ userAuth/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', LoginView.as_view(template_name='userAuth/login.html'), name='login'),
     path('logout/', logout_then_login, {'login_url': 'login'}, name='logout'),
+    path('account/', views.account, name='account'),
+    path('delete/', views.delete, name='delete'),
 ]
--- userAuth/views.py
+++ userAuth/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login
 from .forms import SignUpForm
 from django.urls import reverse
 
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -13,3 +14,20 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'userAuth/signup.html', {'form': form})
+
+def account(request):
+    if request.user.is_authenticated:
+        user = request.user
+        return render(request, 'userAuth/account.html', {'user': user})
+    else:
+        return redirect('login')
+
+def delete(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            user.delete()
+            return redirect('home')
+        return render(request, 'userAuth/delete.html', {'user': user})
+    else:
+        return redirect('delete')
