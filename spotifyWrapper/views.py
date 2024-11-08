51cc20af8c98db31c0cd67d53d48b87454cc7a52
Veer Guda <veerguda@gmail.com>
2024-11-08 04:08:34 -0500

    debugging & fixing authentication/views. Also worked on fixing some edge cases

--- spotifyWrapper/views.py
+++ spotifyWrapper/views.py
@@ -2,12 +2,24 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
 
 def home(request):
     return render(request, 'home.html')
 
 def contact(request):
     return render(request, 'contact.html')
+
+def spotify_callback(request):
+    code = request.GET.get('code')
+    if code:
+        # Process the code (exchange it for an access token, etc.)
+        # Redirect to another page after successful authorization
+        return redirect('/dashboard/')
+    else:
+        return HttpResponse("No authorization code received")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
